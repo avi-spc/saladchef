c45eae69e5d35b335432515bfbbcc983e586a50c
avi-spc <avdhesh7394vn@gmail.com>
2019-08-28 20:30:19 +0530

    Added pick and drop logic :sparkles:

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,24 @@ public class PlayerController : MonoBehaviour
 {
     public int speed;
     public int playerType;
+    public int pickUpsLen;
+
+    public Vector2 rotationAngle;
+
     public Queue pickUps;
 
     public bool chopping;
+
+
+    public GameObject firepoint;
     // Start is called before the first frame update
     void Start() {
         pickUps = new Queue();
+        rotationAngle = Vector2.up;
     }
 
+    private void FixedUpdate() {
+    }
     // Update is called once per frame
     void Update() {
         if (playerType == 1) {
@@ -30,7 +40,16 @@ public class PlayerController : MonoBehaviour
                 if (Input.GetKey(KeyCode.D)) {
                     transform.Translate(Vector2.right * speed * Time.deltaTime);
                 }
+
+                if (Input.GetKeyDown(KeyCode.Q)) {
+                    Pick();
+                }
+
+                if (Input.GetKeyDown(KeyCode.E)) {
+                    Drop();
+                }
             }
+
         }
         else {
             if (Input.GetKey(KeyCode.UpArrow)) {
@@ -46,36 +65,75 @@ public class PlayerController : MonoBehaviour
                 transform.Translate(Vector2.right * speed * Time.deltaTime);
             }
 
-            if (Input.GetKeyDown(KeyCode.Q)) {
-                Pick();
-            }
         }
-        
+            
     }
 
     void Pick() {
+        Debug.DrawRay(firepoint.transform.position, rotationAngle, Color.blue, 10000);
 
+        RaycastHit raycastHit;
+
+        if (Physics.Raycast(firepoint.transform.position, rotationAngle , out raycastHit, 10000f)) {
+            if(raycastHit.collider.gameObject.tag.Equals("Veg"))
+                if (pickUps.Count < 2) {
+                    pickUps.Enqueue(raycastHit.collider.gameObject.name);
+                    pickUpsLen = pickUps.Count;
+                }
+            Debug.Log(pickUps.Count);
+            foreach (var item in pickUps) {
+                Debug.Log(item);
+            }
+        }
     }
 
     void Drop() {
+        Debug.DrawRay(firepoint.transform.position, rotationAngle, Color.red, 10000);
+
+        RaycastHit raycastHit;
+
+        if (Physics.Raycast(firepoint.transform.position, rotationAngle, out raycastHit, 10000f)) {
+            if (raycastHit.collider.gameObject.tag.Equals("ChopBoard")) {
+                Debug.Log(raycastHit.collider.gameObject.tag);
+                if(pickUps.Count > 0)
+                    StartCoroutine(Chopping());
+            }             
+        }
     }
 
     void Throw() {
     }
 
-    private void OnTriggerEnter2D(Collider2D col) {
-        if (col.gameObject.tag.Equals("Veg")) {
-            if (pickUps.Count < 2) {
-                pickUps.Enqueue(col.gameObject.name);
-            }
-
-            Debug.Log(pickUps.Count);
+    private void OnTriggerEnter(Collider col) {
+        if (col.gameObject.name.Equals("Left")) {
+            rotationAngle = Vector2.left;
+            firepoint.transform.localPosition = new Vector2(-0.34f, 0);
         }
 
-        if (col.gameObject.tag.Equals("ChopBoard")) {
-            StartCoroutine(Chopping());
+        if (col.gameObject.name.Equals("Right")) {
+            rotationAngle = Vector2.right;
+            firepoint.transform.localPosition = new Vector2(0.34f, 0);
+        }
+
+        if (col.gameObject.name.Equals("Down")) {
+            rotationAngle = Vector2.down;
+            firepoint.transform.localPosition = new Vector2(0, -0.34f);
+        }
+
+        if (col.gameObject.name.Equals("Up")) {
+            rotationAngle = Vector2.up;
+            firepoint.transform.localPosition = new Vector2(0, 0.34f);
         }
     }
+
+    //private void OnCollisionStay2D(Collision2D col) {
+    //    if (Input.GetKeyDown(KeyCode.C)) {
+    //        if (col.gameObject.tag.Equals("ChopBoard")) {
+    //            StartCoroutine(Chopping());
+    //        }
+    //    }
+    //}
+
 
     IEnumerator Chopping() {
         Debug.Log("chopping");
